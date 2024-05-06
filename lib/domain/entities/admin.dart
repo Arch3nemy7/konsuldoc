4e6e0c2723419af367b826d210b7d96415846ca8
Zander <84573105+Zander5357@users.noreply.github.com>
2024-05-06 13:33:16 +0700

    Merge feat/admin-repository to development (#10)
    * Add: Admin model, entities, repository, & repository impl; Modify .gitignore
    * Change: Admin Repository and Admin Repository Impl
    * Cooking .gitignore
    * Changes: a

--- lib/domain/entities/admin.dart
+++ lib/domain/entities/admin.dart
@@ -2,5 +2,16 @@ import 'package:konsuldoc/domain/entities/user.dart';
 import 'package:konsuldoc/domain/enums/role.dart';
 
 class Admin extends User {
-  Admin({required super.id}) : super(role: Role.admin);
+  final String? avatar;
+  final String email;
+  final String name;
+  final String? phone;
+
+  Admin({
+    required super.id,
+    required this.avatar,
+    required this.email,
+    required this.name,
+    required this.phone,
+  }) : super(role: Role.admin);
 }
