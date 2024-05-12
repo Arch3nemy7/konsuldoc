4b4660d3d2fa654b3fcafaa4820516f8bfc5f4d9
nimasayu26 <nimasayu1526@gmail.com>
2024-05-12 22:31:37 +0700

    feat:ui form admin, dokter, member

---
+++ lib/presentations/pages/form_member.dart
@@ -0,0 +1,229 @@
+import 'package:flutter/material.dart';
+
+enum Gender { Pria, Wanita }
+
+class MemberFormPage extends StatefulWidget {
+  const MemberFormPage({Key? key}) : super(key: key);
+
+  @override
+  State<MemberFormPage> createState() => _MemberFormPageState();
+}
+
+class _MemberFormPageState extends State<MemberFormPage> {
+  Gender? _gender;
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _phoneController = TextEditingController();
+  TextEditingController _addressController = TextEditingController();
+  TextEditingController _dobController = TextEditingController();
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _emailController.dispose();
+    _phoneController.dispose();
+    _addressController.dispose();
+    _dobController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Member'),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(15),
+        child: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: ListView(
+            children: [
+              Center(
+                child: Stack(
+                  children: [
+                    Container(
+                      width: 150,
+                      height: 150,
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 4, color: Colors.white),
+                        boxShadow: [
+                          BoxShadow(
+                            spreadRadius: 2,
+                            blurRadius: 10,
+                            color: Colors.black.withOpacity(0.1),
+                          )
+                        ],
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage(
+                            'https://cdn.pixabay.com/photo/2020/12/13/16/37/woman-5828786_1280.jpg',
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: Container(
+                        height: 40,
+                        width: 40,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.rectangle,
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(width: 4, color: Colors.white),
+                          color: Color.fromRGBO(28, 42, 58, 1),
+                        ),
+                        child: Icon(
+                          Icons.edit,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 30),
+              TextFormField(
+                controller: _nameController,
+                keyboardType: TextInputType.name,
+                decoration: const InputDecoration(
+                  labelText: "Nama",
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.perm_identity_rounded),
+                ),
+                validator: (value) {
+                  if (value!.isEmpty ||
+                      !RegExp(r'^[a-zA-Z ]+$').hasMatch(value)) {
+                    return "Masukkan nama yang valid";
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 15),
+              TextFormField(
+                controller: _emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                  labelText: "Email",
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.email),
+                ),
+                validator: (value) {
+                  if (value!.isEmpty ||
+                      !RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$')
+                          .hasMatch(value)) {
+                    return "Masukkan email yang valid";
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 15),
+              TextFormField(
+                controller: _phoneController,
+                keyboardType: TextInputType.phone,
+                decoration: const InputDecoration(
+                  labelText: "No Telepon",
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.add_call),
+                ),
+                validator: (value) {
+                  if (value!.isEmpty ||
+                      !RegExp(r'^\+?62[0-9]+$').hasMatch(value)) {
+                    return "Masukkan nomor telepon yang valid";
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 15),
+              TextFormField(
+                controller: _addressController,
+                keyboardType: TextInputType.streetAddress,
+                decoration: const InputDecoration(
+                  labelText: "Alamat",
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.home),
+                ),
+              ),
+              SizedBox(height: 15),
+              TextFormField(
+                controller: _dobController,
+                keyboardType: TextInputType.datetime,
+                decoration: const InputDecoration(
+                  labelText: "Tanggal Lahir",
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.calendar_today_sharp),
+                ),
+              ),
+              SizedBox(height: 15),
+              Text(
+                'Jenis Kelamin',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              SizedBox(height: 5),
+              Row(
+                children: [
+                  Expanded(
+                    child: RadioListTile<Gender>(
+                      contentPadding: EdgeInsets.all(0.0),
+                      value: Gender.Pria,
+                      dense: true,
+                      groupValue: _gender,
+                      title: Text(Gender.Pria.toString().split('.').last),
+                      onChanged: (val) {
+                        setState(() {
+                          _gender = val;
+                        });
+                      },
+                    ),
+                  ),
+                  Expanded(
+                    child: RadioListTile<Gender>(
+                      contentPadding: EdgeInsets.all(0.0),
+                      value: Gender.Wanita,
+                      dense: true,
+                      groupValue: _gender,
+                      title: Text(Gender.Wanita.toString().split('.').last),
+                      onChanged: (val) {
+                        setState(() {
+                          _gender = val;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 15),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: ElevatedButton(
+                  child: Text('Simpan'),
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromRGBO(58, 115, 149, 1),
+                    foregroundColor: Colors.white,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
