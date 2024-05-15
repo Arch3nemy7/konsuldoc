ed7d75d137b8776c264b02a2ef65325f7a8378e4
nimasayu26 <nimasayu1526@gmail.com>
2024-05-15 18:24:03 +0700

    feat:poli

--- lib/domain/enums/specialist.dart
+++ lib/domain/enums/specialist.dart
@@ -4,39 +4,74 @@ import 'package:flutter/material.dart';
 // TODO: Isi daftar spesialis / poli
 enum Specialist {
   dental(
-    label: 'Dokter Gigi',
+    label: 'Poli Gigi',
     icon: FluentIcons.dentist_48_regular,
     activeIcon: FluentIcons.dentist_48_filled,
   ),
   pediatrician(
-    label: 'Dokter Anak',
+    label: 'Poli Anak',
     icon: FluentIcons.teddy_24_regular,
     activeIcon: FluentIcons.teddy_24_filled,
   ),
-  dental2(
-    label: 'Poli Gigi',
-    icon: FluentIcons.dentist_48_regular,
-    activeIcon: FluentIcons.dentist_48_filled,
+  General(
+    label: 'Poli Umum',
+    icon: FluentIcons.briefcase_48_regular,
+    activeIcon: FluentIcons.briefcase_48_filled,
   ),
-  dental3(
-    label: 'Poli Gigi',
-    icon: FluentIcons.dentist_48_regular,
-    activeIcon: FluentIcons.dentist_48_filled,
+  Obstetrics(
+    label: 'Poli Kandungan dan Kebidanan ',
+    icon: Icons.pregnant_woman,
+    activeIcon: Icons.pregnant_woman,
   ),
-  dental4(
-    label: 'Poli Gigi',
-    icon: FluentIcons.dentist_48_regular,
-    activeIcon: FluentIcons.dentist_48_filled,
+  Internal_Medicine(
+    label: 'Poli Penyakit Dalam ',
+    icon: Icons.medical_services,
+    activeIcon: Icons.medical_services,
   ),
-  dental5(
-    label: 'Poli Gigi',
-    icon: FluentIcons.dentist_48_regular,
-    activeIcon: FluentIcons.dentist_48_filled,
+  Surgical(
+    label: 'Poli Bedah',
+    icon: FluentIcons.cut_24_regular,
+    activeIcon: FluentIcons.cut_24_filled,
   ),
-  dental6(
-    label: 'Poli Gigi',
-    icon: FluentIcons.dentist_48_regular,
-    activeIcon: FluentIcons.dentist_48_filled,
+  Dematology(
+    label: 'Poli Kulit',
+    icon: Icons.face,
+    activeIcon: Icons.face,
+  ),
+  Eye(
+    label: 'Poli Mata',
+    icon: FluentIcons.people_eye_20_regular,
+    activeIcon: FluentIcons.people_eye_20_filled,
+  ),
+  Ent(
+    label: 'Poli THT',
+    icon: Icons.hearing,
+    activeIcon: Icons.hearing,
+  ),
+  Cardiology(
+    label: 'Poli Jantung',
+    icon: FluentIcons.heart_48_regular,
+    activeIcon: FluentIcons.heart_48_filled,
+  ),
+  Psychiatry(
+    label: 'Poli Psikiatri',
+    icon: Icons.psychology,
+    activeIcon: Icons.psychology,
+  ),
+  Orthopedic(
+    label: 'Poli Orthopedic',
+    icon: FluentIcons.dumbbell_28_regular,
+    activeIcon: FluentIcons.dumbbell_28_filled,
+  ),
+  Urology(
+    label: 'Poli Urologi',
+    icon: FluentIcons.water_24_regular,
+    activeIcon: FluentIcons.water_24_filled,
+  ),
+  Neurology(
+    label: 'Poli Saraf',
+    icon: FluentIcons.brain_circuit_24_regular,
+    activeIcon: FluentIcons.brain_circuit_24_filled,
   );
 
   const Specialist(
