c0ecff2e0c0bd89b602d4f9d10de8c5c8cf81c28
Wisnu Agung Pambudi <wisnu.agung.001@gmail.com>
2024-06-06 22:51:07 +0700

    fix: show session time on doctor appointment list

--- lib/presentations/pages/common/doctor_detail_page.dart
+++ lib/presentations/pages/common/doctor_detail_page.dart
@@ -2,7 +2,6 @@ import 'package:auto_route/auto_route.dart';
 import 'package:expandable_text/expandable_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:fpdart/fpdart.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:konsuldoc/core/constants/constants.dart';
 import 'package:konsuldoc/core/router/member_router.gr.dart';
